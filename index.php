4a3c843948e9651e199249d9143a863dda586166
Sibok <maelstrom@mail.ru>
2021-12-25 13:42:25 +0400

    HW07

---
+++ index.php
@@ -0,0 +1,130 @@
+<?php
+
+/* 1. Найти и указать в проекте Front Controller и расписать классы, которые с ним взаимодействуют. */
+
+/* Фрон контроллером является класс RouteCollection(), который объеденяет в себе все маршруты для взаимодействия с классами 
+MainController, ProductController, OrderController, UserController */
+
+/* 2. Найти в проекте паттерн Registry и объяснить, почему он был применён. */
+
+/* Паттерн находится в architecture/app/framework/Registry.php Он используется для доступа к конфигурационным данным и доступа
+к инстансу UrlGenerator, нужного многим другим объектам. */
+
+/* 3. Добавить во все классы Repository использование паттерна Identity Map вместо постоянного генерирования сущностей. */
+
+class User
+{
+
+    public static $identityMap = [];
+
+    /**
+     * Получаем пользователя по идентификатору
+     *
+     * @param int $id
+     * @return Entity\User|null
+     */
+    public function getById(int $id): ?Entity\User
+    {
+        if(isset(self::$identityMap[$id])){
+            return self::$identityMap[$id];
+        } else {
+            foreach ($this->getDataFromSource(['id' => $id]) as $user) {
+                $resultUser = $this->createUser($user);
+                self::$identityMap[$id] = $resultUser;
+                return $resultUser;
+            }
+            return null;
+        }
+    }
+
+    /**
+     * Получаем пользователя по логину
+     *
+     * @param string $login
+     * @return Entity\User
+     */
+    public function getByLogin(string $login): ?Entity\User
+    {
+        foreach ($this->getDataFromSource(['login' => $login]) as $user) {
+            if ($user['login'] === $login) {
+                return $this->createUser($user);
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Фабрика по созданию сущности пользователя
+     *
+     * @param array $user
+     * @return Entity\User
+     */
+    private function createUser(array $user): Entity\User
+    {
+        $role = $user['role'];
+
+        return new Entity\User(
+            $user['id'],
+            $user['name'],
+            $user['login'],
+            $user['password'],
+            new Entity\Role($role['id'], $role['title'], $role['role'])
+        );
+    }
+
+    /**
+     * Получаем пользователей из источника данных
+     *
+     * @param array $search
+     *
+     * @return array
+     */
+    private function getDataFromSource(array $search = [])
+    {
+        $admin = ['id' => 1, 'title' => 'Super Admin', 'role' => 'admin'];
+        $user = ['id' => 1, 'title' => 'Main user', 'role' => 'user'];
+        $test = ['id' => 1, 'title' => 'For test needed', 'role' => 'test'];
+
+        $dataSource = [
+            [
+                'id' => 1,
+                'name' => 'Super Admin',
+                'login' => 'root',
+                'password' => '$2y$10$GnZbayyccTIDIT5nceez7u7z1u6K.znlEf9Jb19CLGK0NGbaorw8W', // 1234
+                'role' => $admin
+            ],
+            [
+                'id' => 2,
+                'name' => 'Doe John',
+                'login' => 'doejohn',
+                'password' => '$2y$10$j4DX.lEvkVLVt6PoAXr6VuomG3YfnssrW0GA8808Dy5ydwND/n8DW', // qwerty
+                'role' => $user
+            ],
+            [
+                'id' => 3,
+                'name' => 'Ivanov Ivan Ivanovich',
+                'login' => 'i**extends',
+                'password' => '$2y$10$TcQdU.qWG0s7XGeIqnhquOH/v3r2KKbes8bLIL6NFWpqfFn.cwWha', // PaSsWoRd
+                'role' => $user
+            ],
+            [
+                'id' => 4,
+                'name' => 'Test Testov Testovich',
+                'login' => 'testok',
+                'password' => '$2y$10$vQvuFc6vQQyon0IawbmUN.3cPBXmuaZYsVww5csFRLvLCLPTiYwMa', // testss
+                'role' => $test
+            ],
+        ];
+
+        if (!count($search)) {
+            return $dataSource;
+        }
+
+        $productFilter = function (array $dataSource) use ($search): bool {
+            return (bool) array_intersect($dataSource, $search);
+        };
+
+        return array_filter($dataSource, $productFilter);
+    }
+}
